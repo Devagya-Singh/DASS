607162e82fe3b5bd6c9b1a4d6b36adebbee8b31c
Devagya-Singh <devagyarupsingh.com>
2025-10-06 16:04:03 +0530

    Added new team

--- frontend-DASS/src/assets/team.js
+++ frontend-DASS/src/assets/team.js
@@ -1,14 +1,13 @@
-import adrija from './profile_adrija.jpg'
-import ankita from './profile_ankita.jpg'
-import debleena from './profile_deebleena.jpg'
 import devagya from './profile_devagya.jpg'
-import sampurna from './profile_sampurna.jpg'
 import swaralipi from './profile_swaralipi.jpg'
+import naman from './profile_naman.jpg'
+import shahid from './profile_shahid.jpg'
+import mam from './profile_mam.jpg'
 
 const teamMembers = [
     {
       name: "Devagya Singh",
-      role: "Project Lead, Frontend Engineer",
+      role: "Project Lead, Frontend Engineer, Backend Developer",
       image: devagya // Ensure this path is correct
     },
     {
@@ -17,25 +16,20 @@ const teamMembers = [
       image: swaralipi
     },
     {
-      name: "Sampurna Sen",
-      role: "Technical Writer",
-      image: sampurna
+      name: "Naman Deo",
+      role: "Frontend Assistance and Testing",
+      image: naman
     },
     {
-      name: "Debleena Biswas",
-      role: "UI/UX Designer",
-      image: debleena
+      name: "Abdul Shahid",
+      role: "Documentation and Testing",
+      image: shahid
     },
     {
-      name: "Adrija Das",
-      role: "QA & Testing",
-      image: adrija
-    },
-    {
-        name: "Ankita Mohanty",
-        role: "QA & Testing",
-        image: ankita
-      }
+      name: "Lipika Mohanty",
+      role: "Guidance and Mentoring",
+      image: mam
+    }
   ];
   
   export default teamMembers;
